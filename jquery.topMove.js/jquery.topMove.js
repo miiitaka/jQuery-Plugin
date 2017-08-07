16b0a217000f2be3978d67f3cf08dabae976939d
Kazuya Takami <foundationmeister@outlook.com>
2017-08-07 19:11:43 +0900

    First commit.

---
+++ jquery.topMove.js/jquery.topMove.js
@@ -0,0 +1,29 @@
+;(function($){
+  $.fn.topMove = function(options){
+    var defaults = {
+      "speed"  : 500,
+      "scroll" : 300
+    };
+    var
+      settings = $.extend( {}, defaults, options ),
+      event = "ontouchend" in window ? "touchend" : "click";
+
+    return this.each(function(){
+      var self = $(this);
+      self.hide();
+
+      self.on(event, function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop: 0}, settings.speed);
+      });
+
+      $(window).on('scroll', function () {
+        if ($(this).scrollTop() > settings.scroll) {
+          self.fadeIn();
+        } else {
+          self.fadeOut();
+        }
+      });
+    });
+  };
+}(jQuery));
